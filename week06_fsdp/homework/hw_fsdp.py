f38c95f3cd9efd730b7623c42c89c791fa6bc769
Boris Sharchilev <sharch@meta.com>
2025-04-23 12:11:14 -0700

    Trying to get pytorch to work on runpod

--- week06_fsdp/homework/hw_fsdp.py
+++ week06_fsdp/homework/hw_fsdp.py
@@ -422,7 +422,7 @@ def post_backward(module: FSDPModule):
                 for fsdp_param in module.fsdp_params:
                     if not fsdp_param.sharded_param.requires_grad:
                         continue
-                    print(f"module {module._module_fqn}, param shape {fsdp_param.sharded_param.shape}, has grad {hasattr(fsdp_param.sharded_param, 'grad')}, requires grad {fsdp_param.sharded_param.requires_grad}, has unsharded param grad {hasattr(fsdp_param._unsharded_param, 'grad')}, state {fsdp_param.sharded_state}")
+                    print(f"module {module._module_fqn}, param shape {fsdp_param.sharded_param.shape}, has grad {hasattr(fsdp_param.sharded_param, 'grad')}, requires grad {fsdp_param.sharded_param.requires_grad}, has unsharded param grad {hasattr(fsdp_param._unsharded_param, 'grad')}, type {type(fsdp_param._unsharded_param.grad)}, state {fsdp_param.sharded_state}")
                     fsdp_param.sharded_param.grad = torch.empty_like(fsdp_param.sharded_param)
                     torch.distributed.reduce_scatter_tensor(fsdp_param.sharded_param.grad, fsdp_param._unsharded_param.grad, async_op=True)
                 fsdp_param._unsharded_param.grad.storage().resize_(0)
