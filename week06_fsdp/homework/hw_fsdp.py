75ab214bd15fa0dabf7c70d4fc0faf20c607eb62
Boris Sharchilev <sharch@meta.com>
2025-04-22 18:48:49 -0700

    Trying to get pytorch to work on runpod

--- week06_fsdp/homework/hw_fsdp.py
+++ week06_fsdp/homework/hw_fsdp.py
@@ -426,6 +426,7 @@ def post_backward(module: FSDPModule):
             with torch.cuda.stream(module.comm_ctx.reduce_scatter_stream):
                 for fsdp_param in module.fsdp_params:
                     fsdp_param.sharded_param.grad = torch.empty_like(fsdp_param.sharded_param)
+                    print(fsdp_param._unsharded_param.grad.shape)
                     torch.distributed.reduce_scatter_tensor(fsdp_param.sharded_param.grad, fsdp_param._unsharded_param.grad, async_op=True)
                 fsdp_param._unsharded_param.grad.storage().resize_(0)
                 module._post_reduce_event = torch.cuda.Event()
